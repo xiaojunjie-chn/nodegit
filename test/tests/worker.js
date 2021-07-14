ad5ff45cad94ac48fdf74308e8f22941a8fa45db
Alex Aveillán <alexa@axosoft.com>
2021-07-14 09:10:13 +0200

    test: no terminate and give enough time
    GC seems to work

--- test/tests/worker.js
+++ test/tests/worker.js
@@ -11,16 +11,31 @@ try {
 
 if (Worker) {
   describe("Worker", function() {
+
+    function delay(t, v) {
+      return new Promise(function(resolve) { 
+          setTimeout(resolve.bind(null, v), t);
+      });
+    }
+
+    
     const clonePath = local("../repos/clone");
 
+    console.log("pid", process.pid);
+    
     // Set a reasonable timeout here now that our repository has grown.
     this.timeout(30000);
 
     beforeEach(function() {
-      return fse.remove(clonePath).catch(function(err) {
-        console.log(err);
 
-        throw err;
+      // return delay(14000)
+      return delay(1)
+      .then(function() {
+        return fse.remove(clonePath).catch(function(err) {
+          console.log(err);
+  
+          throw err;
+        });
       });
     });
 
@@ -52,7 +67,7 @@ if (Worker) {
       });
     });
 
-    for (let i = 0; i < 5; ++i) {
+    for (let i = 0; i < 2; ++i) {
       it.only(`can kill worker thread while in use #${i}`, function(done) { // jshint ignore:line
         const workerPath = local("../utils/worker.js");
         const worker = new Worker(workerPath, {
@@ -64,17 +79,25 @@ if (Worker) {
         worker.on("message", (message) => {
           switch (message) {
             case "init":
+              console.log("main on init\n");
               break;
             case "success":
-              worker.terminate();
+              console.log("main on success\n");
+              // worker.terminate();
               break;
             case "failure":
+              console.log("main on failure\n");
               assert.fail();
               break;
           }
         });
-        worker.on("error", () => assert.fail());
-        worker.on("exit", (code) => {
+        worker.on("error", () => {
+          console.log("main on error\n");
+          assert.fail();
+        });
+        worker.on("exit", (code, signal) => {
+          console.log("main on exit with code", code);
+          console.log(" and signal ", signal, "\n");
           if (code === 1) {
             done();
           } else {
